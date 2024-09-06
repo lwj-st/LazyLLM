53497b6c5423db6c92ae5ba35149a87ba695dc5f
lwj-st <liwenjian@sensetime.com>
2024-09-06 17:13:57 +0800

    test

--- tests/lwj/test_2.py
+++ tests/lwj/test_2.py
@@ -4,6 +4,6 @@ import time
 def test_random_number():
     random_number = random.randint(1, 10)  # 生成1到10之间的随机数
     print(f"Generated random number: {random_number}")
-    time.sleep(2)
+    time.sleep(10)
     assert random_number >= 5, f"Test failed: The random number {random_number} is less than 5"
 
