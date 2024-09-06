bb11ed2e148d90bb24ee591984e664b92139ac8b
lwj-st <liwenjian@sensetime.com>
2024-09-06 17:11:17 +0800

    test

--- tests/lwj/test_3.py
+++ tests/lwj/test_3.py
@@ -5,7 +5,6 @@ def test_random_number():
     print(f"Generated random number: {random_number}")
     assert random_number >= 5, f"Test failed: The random number {random_number} is less than 5"
 
-
 def test_random_number2():
     random_number = random.randint(1, 10)  # 生成1到10之间的随机数
     print(f"Generated random number: {random_number}")
